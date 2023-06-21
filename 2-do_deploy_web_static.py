92b6d7239769e05edd363f732adc604b61f55bbf
christauwicyeza <c.uwiwcyeza@alustudent.com>
2023-06-21 14:02:19 +0200

    fixed errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,7 +13,7 @@ env.hosts = ['35.172.220.65', '34.204.53.150']
 env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    
+
     """deploy web static"""
     if not exists(archive_path):
         return False
@@ -29,6 +29,11 @@ def do_deploy(archive_path):
         run("rm -rf {}/web_static".format(path_name))
         run('rm -rf /data/web_static/current')
         run('ln -s {}/ /data/web_static/current'.format(path_name))
+
+        if not isdir("/var/www/html/hbnb_static"):
+            run("sudo mkdir -p /var/www/html/hbnb_static")
+
+        run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
         return True
     except Exception:
         return False
